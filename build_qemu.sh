92ad792d9c479de9a699ab1ffaff498f18834199
Wilson Bo <wilsonbo@leansoftx.com>
2024-01-27 21:33:55 +0800

    1

--- build_qemu.sh
+++ build_qemu.sh
@@ -4,6 +4,7 @@ docker run -it -v .:/root/tbox/ tbox-compiler:v4
 
 #重新生成ext4，将编译后文件放入
 cd ..
+pwd
 mount ./qemu/docker/files/qemufiles/initrd_32le_v2.ext4 ~/tmp
 rm -rf  ~/tmp/iov/
 mkdir ~/tmp/iov/
