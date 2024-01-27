d3bc31f408f9c1ff2efa413e63a415398439651a
Wilson Bo <wilsonbo@leansoftx.com>
2024-01-27 21:31:30 +0800

    1

--- cantomqtt/make.sh
+++ cantomqtt/make.sh
@@ -1,4 +1,5 @@
 cmake . 
 arm-linux-gnueabi-gcc writecan.c common/conversion.c -o can_write -ljansson -march=armv5te
 arm-linux-gnueabi-gcc main.c common/conversion.c -o can_mqtt -lpaho-mqtt3c -ljansson -march=armv5te
-chmod -R 777 cantomqtt
+chmod -R 777 can_mqtt
+chmod -R 777 can_write
