302eee0760e20871c96664950c8084658ea9719f
wilsonbo <wilsonbo@lean-soft.cn>
2024-01-27 11:37:09 +0000

    Updated make.sh

--- cantomqtt/make.sh
+++ cantomqtt/make.sh
@@ -1,3 +1,4 @@
 cmake . 
+arm-linux-gnueabi-gcc writecan.c common/conversion.c -o can_write -ljansson -march=armv5te
 arm-linux-gnueabi-gcc main.c common/conversion.c -o can_mqtt -lpaho-mqtt3c -ljansson -march=armv5te
 chmod -R 777 cantomqtt
