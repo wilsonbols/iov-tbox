c04680e65eb9ceccf80f20d6df236de02a15dc75
wilsonbo <wilsonbo@lean-soft.cn>
2024-01-29 01:45:05 +0000

    Updated build_qemu.sh

--- build_qemu.sh
+++ build_qemu.sh
@@ -1,6 +1,7 @@
 #交叉编译c程序
+docker login -u cn-north-4@XVLLZV9P2LDL1Q47R2R5 -p 2206da3a9b99abd4bfaae36f42f590512f638e4d3125bb0c5be0e35816704941 swr.cn-north-4.myhuaweicloud.com
 cd ~/IoV-Edge-CClient/cantomqtt
-docker run -it -v .:/root/tbox/ tbox-compiler:v4
+docker run -it -v .:/root/tbox/ swr.cn-north-4.myhuaweicloud.com/iov-workshop/tbox-compiler:v4
 
 #重新生成ext4，将编译后文件放入
 mount ~/qemu/docker/files/qemufiles/initrd_32le_v2.ext4 ~/tmp
