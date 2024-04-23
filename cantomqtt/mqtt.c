3b162ceeb47127de62a19fca64befaf5200ce786
wilsonbo <ade08f6abfb14ef1b512e48191cfb67a@example.com>
2024-04-23 11:39:17 +0800

    更新 mqtt.c "tcp://192.168.181.128:1883"

--- cantomqtt/mqtt.c
+++ cantomqtt/mqtt.c
@@ -8,7 +8,7 @@
 
 #define DEBUG       0
 
-#define ADDRESS     "tcp://10.247.132.112:1883"
+#define ADDRESS     "tcp://192.168.181.128:1883"
 #define USERNAME    "admin"
 #define PASSWORD    "public"
 #define CLIENTID    "c-client-vehicle"
