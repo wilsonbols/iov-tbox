1d818c9adf9f0932bf1d137f84a957b9d97c9d94
lixiaoming <lixiaoming@leansoftx.com>
2024-06-12 10:43:40 +0800

    1

--- build_gtest.sh
+++ build_gtest.sh
@@ -2,51 +2,86 @@
 pwd
 ls
 
-# apt-get update && apt upgrade -y
+apt-get update && apt upgrade -y
 
-# apt-get -y install wget
-# apt-get -y install libpaho-mqtt-dev
-# apt-get -y install libjansson-dev
-# apt-get -y install libgtest-dev
-# apt-get -y install lcov
+apt-get -y install wget
+apt-get -y install libpaho-mqtt-dev
+apt-get -y install libjansson-dev
+apt-get -y install libgtest-dev
+apt-get -y install lcov
 
-# apt install ca-certificates apt-transport-https software-properties-common lsb-release libboost-dev git -y
+apt install ca-certificates apt-transport-https software-properties-common lsb-release libboost-dev git -y
 
-# apt install python3.11 -y
-# python3.11 --version
+apt install python3.11 -y
+python3.11 --version
 
-# echo "----------install deps end ----------"
+echo "----------install deps end ----------"
 
-# echo "----------start build and intstall mockcpp ----------"
+echo "----------start build and intstall mockcpp ----------"
 
-# rm -rf ../mockcpp
+rm -rf ../mockcpp
 
-# git clone https://github.com/sinojelly/mockcpp.git ../mockcpp
+git clone https://github.com/sinojelly/mockcpp.git ../mockcpp
 
 
-# cd ../mockcpp
+cd ../mockcpp
 
+sed -i 's/XUNIT_NAME=testngpp/XUNIT_NAME=gtest/g' build_install.sh
 # sed -i 's/XUNIT_NAME=testngpp/XUNIT_NAME=gtest/g' build_install.sh
-# # sed -i 's/XUNIT_NAME=testngpp/XUNIT_NAME=gtest/g' build_install.sh
 
-# echo "----------cat mockcpp build_install.sh content  ----------"
-# cat build_install.sh
-# echo "----------cat mockcpp build_install.sh content End. will build mockcpp ----------"
+echo "----------cat mockcpp build_install.sh content  ----------"
+cat build_install.sh
+echo "----------cat mockcpp build_install.sh content End. will build mockcpp ----------"
 
-# XUNIT_HOME=/usr/src/gtest ./build_install.sh # gtest intall to this dir
-# ls -ll ../
-
-# echo "----------start build tbox ----------"
-# cd ../test_tools/mockcpp_install
-# cp lib/libmockcpp.a /usr/lib
-# cp -rf ./include/mockcpp /usr/include/mockcpp
-# cp -rf ./include/fake_boost /usr/include/fake_boost
-# cp -rf ./include/msinttypes /usr/include/msinttypes
-
-
-## cd /root/workspace/IoV-TBox
+XUNIT_HOME=/usr/src/gtest ./build_install.sh # gtest intall to this dir
+ls -ll ../
 
 echo "----------start build tbox ----------"
+cd ../test_tools/mockcpp_install
+cp lib/libmockcpp.a /usr/lib
+cp -rf ./include/mockcpp /usr/include/mockcpp
+cp -rf ./include/fake_boost /usr/include/fake_boost
+cp -rf ./include/msinttypes /usr/include/msinttypes
+
+
+echo "----------build tbox app for arm platform start -----------------------"
+
+cd /root/tbox/cantomqtt
+
+pwd
+
+can_send="./can_send"
+tbox="./tbox"
+
+# check file exist
+if [ -e "$can_send" ]; then
+    rm -f "$can_send"
+    echo "can_send deleted."
+else
+    echo "File does not exist."
+fi
+
+
+if [ -e "$tbox" ]; then
+    rm -f "$tbox"
+    echo "tbox deleted."
+else
+    echo "File does not exist."
+fi
+
+echo "----------build start------------------------------------------------"
+
+# local debug
+arm-linux-gnueabi-gcc -g -o can_send writecan.c control.c -ljansson -march=armv5te
+
+# -g for gdb remote debug 
+arm-linux-gnueabi-gcc -g -o tbox mqtt.c control.c collision.c -lpaho-mqtt3c -ljansson -march=armv5te
+chmod -R 777 can_send
+chmod -R 777 tbox
+
+echo "----------build tbox app for arm platform end -----------------------"
+
+echo "----------start build tbox for unit test ----------"
 
 cd /root/tbox
 
